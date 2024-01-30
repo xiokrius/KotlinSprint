ad93e4bcdf625112a458522afd82eb418611ff32
xiokrius <xiokriusa@gmail.com>
2024-01-30 23:39:36 +0300

    KS-1-1 completely ready solution

--- src/main/kotlin/lesson_1/lesson1_task1.kt
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -1,5 +1,11 @@
-package lesson_1
-
 fun main () {
-    println("Hello World!")
+
+    /* Решение 1 таски */
+    val TheNumberOfOrders = 75
+    val Greetings = "Thank you for your purchase"
+
+    /* Решение 2 таски */
+    val One_SideA: String = ("height")
+    val Two_SideB: String = ("Width")
+
 }

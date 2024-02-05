0c83aaa13c0e1539ddd1b27be10a92737787bc2c
xiokrius <xiokriusa@gmail.com>
2024-02-05 21:12:21 +0300

    KS-1-1  finally

--- src/main/kotlin/lesson_1/lesson1_task1.kt
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -1,11 +1,8 @@
 fun main () {
 
-    /* Решение 1 таски */
     val TheNumberOfOrders = 75
     val Greetings = "Thank you for your purchase"
 
-    /* Решение 2 таски */
     val One_SideA: String = ("height")
     val Two_SideB: String = ("Width")
-
 }

9ce8a4ada2cf62de48a66a338a39ade1ad959297
xiokrius <xiokriusa@gmail.com>
2024-02-05 21:24:59 +0300

    KS-1-5 finally(amount of time Gagarin spent in space)

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,15 +1,13 @@
 fun main() {
 
-    val secondsInSpace = 6360
-
-    val hours: Int = secondsInSpace / 3600
-
-    val remainingSecondsAfterHours: Int = secondsInSpace % 3600
-
-    val minutes: Int = remainingSecondsAfterHours / 60
-
-    val seconds: Int = remainingSecondsAfterHours % 60
-
+    val secondsInSpace = 6480
+    val OneMinSec = 60
+    val OneHourMin = 60
+    val OneHourSec = OneMinSec*OneHourMin
+    val hours: Int = secondsInSpace / OneHourSec
+    val remainingSecondsAfterHours: Int = secondsInSpace % OneHourSec
+    val minutes: Int = remainingSecondsAfterHours / OneHourMin
+    val seconds: Int = remainingSecondsAfterHours % OneMinSec
     val formattedTime = String.format("%02d:%02d:%02d", hours, minutes, seconds)
 
         println(formattedTime)
